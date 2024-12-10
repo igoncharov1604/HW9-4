fe0ca79fc01c1cc9c00974c33682f359521bfcaa
igoncharov1604 <i.goncharov@istu.edu.ua>
2024-12-10 18:31:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Масив із 10 елементів (додатні та від'ємні числа)
+        int[] numbers = { -10, 25, -7, 12, 0, -3, 15, -8, 30, -1 };
+
+        // Блоковий лямбда-вираз
+        Func<int, bool> isPositive = s =>
+        {
+            if (s > 0)
+                return true;
+            else
+                return false;
+        };
+
+        // Введення числа s з консолі
+        Console.Write("Введіть число s: ");
+        if (int.TryParse(Console.ReadLine(), out int s))
+        {
+            // Перевірка, чи число додатнє
+            bool result = isPositive(s);
+
+            // Вивід результату
+            Console.WriteLine($"Число {s} є додатнім: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Введено некоректне число.");
+        }
+    }
+}
